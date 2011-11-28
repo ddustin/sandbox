203d108e2fc70dd7cf4c02c2c2de88a3b9ec341c
jatchili <jeff_lim@mit.edu>
2011-11-28 10:45:45 -0800

    Cleaned up code a little bit
    Deleted redundant methods, and added "pragma mark"s. No significant changes.

---
+++ SettingsNavigator/Project3/SettingsCategories.h
@@ -0,0 +1,13 @@
+//
+//  SettingsCategories2.h
+//  Project3
+//
+//  Created by Jeffrey Lim on 11/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsCategories2 : UIViewController
+
+@end
